c4ad2b7db7a1ed2dae98cafa3405823f3633b466
juckchang <c2w2m2@kaist.ac.kr>
2023-02-23 18:40:02 +0900

    [Edit] rule 코드 링크

--- pages/rule.js
+++ pages/rule.js
@@ -33,7 +33,7 @@ export default function Rule() {
           <p>2. 점수제로 순위를 매기며, 동점의 경우 남은 시간을 비교하여 순위를 매깁니다.</p>
           <p>3. 상위 3명한테는 상품이 지급될 예정입니다.</p>
           <p>4. 해킹등을 통하여 점수를 취득하셔도 됩니다.</p>
-          <p>5. 해당 사이트의 소스코드는 <a target="_blank" href="https://github.com/juckchang" style={{ color: 'rgb(30 200 235)' }}>여기</a>에 공개되어있습니다.</p>
+          <p>5. 해당 사이트의 소스코드는 <a target="_blank" href="https://github.com/juckchang/GoN-club-fair" style={{ color: 'rgb(30 200 235)' }}>여기</a>에 공개되어있습니다.</p>
         </Box>
         <br/>
         <Button color="primary" variant="text" startIcon={<HomeIcon />} sx={hoverStyle} onClick={e => handleClick(e, "/")}>
